ffac7e2ededd0b6ab9c5a589403ad7140e2b1e3e
Mehul Rana <86463576+mehulrana017@users.noreply.github.com>
2023-07-03 16:03:46 +0530

    Day-4 with docker(-Adding mongo container -connecting mongo & express container -persisting data within mongo container -network commands)

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+mongoose
+  .connect("mongodb://mehul:mypassword@mongo:27017/?authSource=admin")
+  .then(() => console.log("Database connected"))
+  .catch((e) => console.log(e));
+
 app.get("/", (req, res) => {
-  res.send("<h2>Hi There</h2>");
+  res.send("<h2>Hi There!!!</h2>");
 });
 
 app.listen(port, () => console.log(`listening on ${port}`));
