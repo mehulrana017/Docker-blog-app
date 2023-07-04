d942659bd64a7e1349f15d2a9b5a2cb8ffb50183
Mehul Rana <86463576+mehulrana017@users.noreply.github.com>
2023-07-04 22:34:55 +0530

    Day-5 with Docker (-added config.js -used depends_on to set priority of containers spin-off -added code for managing error for db connection)

---
+++ conifg/config.js
@@ -0,0 +1,7 @@
+export const envs = {
+  MONGO_IP: process.env.MONGO_IP || "mongo",
+  MONGO_PORT: process.env.MONGO_PORT || 27017,
+  MONGO_USER: process.env.MONGO_USER,
+  MONGO_PASSWORD: process.env.MONGO_PASSWORD,
+  PORT: process.env.PORT || 3000,
+};
--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 import express from "express";
 import mongoose from "mongoose";
+import { envs } from "./conifg/config.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-mongoose
-  .connect("mongodb://mehul:mypassword@mongo:27017/?authSource=admin")
-  .then(() => console.log("Database connected"))
-  .catch((e) => console.log(e));
+const { MONGO_IP, MONGO_PORT, MONGO_PASSWORD, MONGO_USER, PORT } = envs;
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("Database connected"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
   res.send("<h2>Hi There!!!</h2>");
 });
 
-app.listen(port, () => console.log(`listening on ${port}`));
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
